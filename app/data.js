06b3205971a877619083abc91c0fedbba659ad94
dev-ahmed-sakib <perfectsakiib.gmail.com>
2024-06-08 16:08:49 +0600

    porto team data

---
+++ app/data.js
@@ -0,0 +1,8 @@
+const team = [
+    {
+        name:"Sakib",
+        position:"Developer",
+        description:"As Salamu-alaikum, my name is sakib, im a mern stack developer",
+        photo:"https://scontent.fdac144-1.fna.fbcdn.net/v/t39.30808-6/361943361_825238125785234_8812670503348895188_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=4AERK-Hur_sQ7kNvgGdQyn6&_nc_ht=scontent.fdac144-1.fna&oh=00_AYDDA6Q0JekcUwIQTUuo9WqPvWAnYVmTXsYvLhpSQkNiAw&oe=66698209"
+    }
+]
---
+++ app/main.js
@@ -0,0 +1,30 @@
+const basicTeam = document.getElementById('basicTeam')
+
+basicTeamMembers = "";
+
+team.map((item, index)=>{
+    basicTeamMembers = `	
+    <div class="col-12 col-sm-6 col-lg-3 isotope-item leadership">
+	<span class="thumb-info thumb-info-hide-wrapper-bg mb-4">
+	<span class="thumb-info-wrapper">
+	<a href="about-me.html">
+	<img src="${item.photo}" class="img-fluid" alt="">
+	<span class="thumb-info-title">
+	<span class="thumb-info-inner">${item.name}</span>
+	<span class="thumb-info-type">${item.position}</span>
+	</span>
+	</a>
+	</span>
+	<span class="thumb-info-caption">
+	<span class="thumb-info-caption-text">${item.description}</span>
+	<span class="thumb-info-social-icons mb-4">
+	<a target="_blank" href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i><span>Facebook</span></a>
+	<a href="http://www.linkedin.com/"><i class="fab fa-linkedin-in"></i><span>Linkedin</span></a>
+	</span>
+	</span>
+	</span>
+	</div>
+    `
+})
+
+basicTeam.innerHTML = basicTeamMembers;
